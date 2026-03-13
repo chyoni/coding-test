ce46dc7c417cf3cb484ae790a6f1a33eedbb7fad
chyonee <chiwon99881@gmail.com>
2026-03-14 08:56:00 +0900

    2026-03-14

---
+++ src/main/java/cwchoiit/solutions/solution2026/march/Solution260314.java
@@ -0,0 +1,57 @@
+package cwchoiit.solutions.solution2026.march;
+
+/**
+ * [이진 탐색]
+ * Description:There is an integer array nums sorted in ascending order (with distinct values).
+ * Prior to being passed to your function,
+ * nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length).
+ * <p>
+ * For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+ * Given the array nums after the rotation and an integer target,
+ * return the index of target if it is in nums, or -1 if it is not in nums.
+ * You must write an algorithm with O(log n) runtime complexity.
+ * <p>
+ * Example 1:
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ * <p>
+ * Example 2:
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ */
+public class Solution260314 {
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        int answer = solution(nums, target);
+        System.out.println(answer);
+    }
+
+    private static int solution(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}
